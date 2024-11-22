b7d1891086e3e20df06dacdf4523467a2ca34244
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-11-21 22:27:42 -0600

    Archivo -- Navegación
    Ahora solo esta listo, el menu de ejemplo de front end

---
+++ frontend/src/plantilla/Navegacion.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export default function Navegacion() {
+    return (
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Tienda App</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link" aria-current="page" href="/">Inicio</a>
+                            <Link className="nav-link" to="/agregar">Agregar Categoria</Link>
+
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
